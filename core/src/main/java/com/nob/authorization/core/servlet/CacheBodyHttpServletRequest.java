4c6e16189bbf5b667f6b916e5fe4e4709a738bf0
truong.ngo.xuan.dev <truong.ngo.xuan.dev@gmail.com>
2025-04-10 14:16:44 +0700

    add

---
+++ core/src/main/java/com/nob/authorization/core/servlet/CacheBodyHttpServletRequest.java
@@ -0,0 +1,31 @@
+package com.nob.authorization.core.servlet;
+
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import org.springframework.util.StreamUtils;
+
+import java.io.*;
+
+public class CacheBodyHttpServletRequest extends HttpServletRequestWrapper {
+
+    private final byte[] cachedBody;
+
+
+    public CacheBodyHttpServletRequest(HttpServletRequest request) throws IOException {
+        super(request);
+        InputStream requestInputStream = request.getInputStream();
+        this.cachedBody = StreamUtils.copyToByteArray(requestInputStream);
+    }
+
+    @Override
+    public ServletInputStream getInputStream() {
+        return new CacheBodyServletInputStream(this.cachedBody);
+    }
+
+    @Override
+    public BufferedReader getReader() {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.cachedBody);
+        return new BufferedReader(new InputStreamReader(byteArrayInputStream));
+    }
+}
---
+++ core/src/main/java/com/nob/authorization/core/servlet/CacheBodyServletInputStream.java
@@ -0,0 +1,58 @@
+package com.nob.authorization.core.servlet;
+
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletInputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CacheBodyServletInputStream extends ServletInputStream {
+
+    private static final Logger log = LoggerFactory.getLogger(CacheBodyServletInputStream.class);
+
+    private final InputStream cachedBodyInputStream;
+
+    public CacheBodyServletInputStream(byte[] cacheBody) {
+        this.cachedBodyInputStream = new ByteArrayInputStream(cacheBody);
+    }
+
+    /**
+     * Indicates whether InputStream has more data to read or not.
+     *
+     * @return true when zero bytes available to read
+     */
+    @Override
+    public boolean isFinished() {
+        try {
+            return cachedBodyInputStream.available() == 0;
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+        return false;
+    }
+
+
+    /**
+     * Indicates whether InputStream is ready for reading or not.
+     * Since we've already copied InputStream in a byte array, we'll return true to indicate that it's always available.
+     *
+     * @return true
+     */
+    @Override
+    public boolean isReady() {
+        return true;
+    }
+
+    @Override
+    public void setReadListener(ReadListener listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int read() throws IOException {
+        return cachedBodyInputStream.read();
+    }
+}
